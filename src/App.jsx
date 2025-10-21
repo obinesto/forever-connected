9c169b034b68dd31178c25990cefb4e5ce71fa56
Cyprian Obi <obicyprian180@gmail.com>
2025-10-21 21:51:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Preloader from "./components/preloader";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
